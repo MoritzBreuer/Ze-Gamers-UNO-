c01664701be5058a356605bcc3436a3d1073a5c0
MoritzBreuer <moritz.breuer@smail.emt.h-brs.de>
2026-01-05 10:44:41 +0100

    Add files via upload

---
+++ UNOSpieler.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class UNOSpieler {
+
+    private String name;
+    private Handkarten kartenAufHand;
+
+    public UNOSpieler(String name) {
+        this.name = name;
+        this.kartenAufHand = new Handkarten();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Handkarten getKartenAufHand() {
+        return kartenAufHand;
+    }
+
+    // Spieler wählt eine Farbe (z.B. bei Farbwechselkarte)
+    public Farbe farbeAuswaehlen() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(name + ", wähle eine Farbe (ROT, GELB, GRUEN, BLAU):");
+
+        while (true) {
+            try {
+                return Farbe.valueOf(scanner.nextLine().toUpperCase());
+            } catch (IllegalArgumentException e) {
+                System.out.println("Ungültige Farbe, bitte erneut eingeben:");
+            }
+        }
+    }
+
+    // Spieler sagt UNO
+    public void unoSagen() {
+        if (kartenAufHand.anzahlKarten() == 1) {
+            System.out.println(name + " sagt: UNO!");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Hallo");
+    }
+
+
+}
+
+
